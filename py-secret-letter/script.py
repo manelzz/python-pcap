cc86eb856d54bac40a2f119415030079247e166e
manelzz <manel.mdz@gmail.com>
2022-03-03 23:35:12 +0100

    permit lowecase

--- py-secret-letter/script.py
+++ py-secret-letter/script.py
@@ -16,7 +16,7 @@ def main():
     print("*" * 150)
 
     player_name = input("Hello! What's your name? ")
-    player_letter = input(f"Welcome, {player_name}! I'm thinking a letter between A and Z, Can you guess it? ")
+    print(f"Welcome, {player_name}! I'm thinking a letter between A and Z, Can you guess it? ")
 
     secret_letter = random.choice(string.ascii_uppercase)
     num_attempts = 0
@@ -27,19 +27,22 @@ def main():
         num_attempts += 1
 
         print("-" * 100)
-        player_number = input(f"Attempt {num_attempts} > Please {player_name}, select a letter  between A-Z" )
+        player_letter = input(f"Attempt {num_attempts} > Please {player_name}, select a letter  between A-Z: " )
         print("-" * 100)
 
-        if player_number == secret_letter:
-            print(f"Congratulations {player_name}! The secret letter  was {secret_letter} and you guessed it at attempt {num_attempts}.")
+        #Capitalize the letter
+        player_letter = player_letter.upper()
+
+        if player_letter == secret_letter:
+            print(f"Congratulations {player_name}! The secret letter was {secret_letter} and you guessed it at attempt {num_attempts}.")
             secret_found = True
-        elif player_number < secret_letter:
+        elif player_letter < secret_letter:
             print(f"Sorry {player_name}! Your guess is to low:")
-        elif player_number > secret_letter:
+        elif player_letter > secret_letter:
             print(f"Sorry {player_name}! Your guess is to high:")
 
-    print(f"Good job {player_name}! Thanks for playing with us")
+    print(f"Good job! Thanks for playing with us")
 
 #Call the main method
 if __name__ == "__main__":
-main()
+    main()
